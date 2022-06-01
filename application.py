83f610cd6bf2772555411cb5ae5b5d1b98c45431
menhquann <102190284@sv1.dut.udn.vn>
2022-06-01 18:47:00 +0700

    test

--- application.py
+++ application.py
@@ -9,7 +9,8 @@ from src.lp_recognition import E2E
 
 # urlCamera = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/262719541_1245270879292641_3943295472295597787_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=aee45a&_nc_ohc=AbrqOdmxL7IAX9YGc5D&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKgFGOti5k9o0DJ9xdnokGZmwm_umUtUzuVck0ODYQHrA&oe=6295B5C6'
 # urlCamera = 'https://icdn.dantri.com.vn/zoom/1200_630/2019/05/18/loat-xe-may-bien-so-dep-gia-sieu-dat-tuan-qua-1-1558138698605.jpg'
-urlCamera = "https://383f-2402-800-6294-656-adbb-796-bbb-e868.ngrok.io/cam-hi.jpg"
+urlCamera = "https://6993-2402-800-6294-9b5c-95f6-ede4-d3-2657.ngrok.io/cam-hi.jpg"
+# https://6993-2402-800-6294-9b5c-95f6-ede4-d3-2657.ngrok.io
 
 # urlCamera= 'http://127.0.0.1:5013/video_feed'
 application = Flask(__name__)
