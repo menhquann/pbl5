835f31973dd940898894122e7cf888e67b0416bd
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2022-06-29 15:41:02 +0700

    up

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ from src.lp_recognition import E2E
 
 # https://6993-2402-800-6294-9b5c-95f6-ede4-d3-2657.ngrok.io
 
-urlCamera= "https://8727-2402-800-6294-9b5c-2537-65c3-c52e-89f5.ap.ngrok.io/cam-hi.jpg"
+urlCamera= "https://3082-2402-800-6294-9b5c-711f-e2bb-adad-3af3.ap.ngrok.io/cam-hi.jpg"
 
 application = Flask(__name__)
 
